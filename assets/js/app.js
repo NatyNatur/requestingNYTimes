37fd6b08db979998ae6f7d7411bd76e7a2a92120
NatyNatur <naticcg@gmail.com>
2018-02-14 11:28:17 -0300

    agregando funcionalidad

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedText;
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML= '';
+  searchedText = searchField.value;
+  getNews();
+});
+
+function getNews() {
+  //se crea el nuevo objeto
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedText}&api-key=e96c1ec17753448eac5a6e2e5f7a2592`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  //se envía la solicitud
+  articleRequest.send();
+}
+
+function handleError() {
+  console.log('Se ha presentado un error');
+}
+
+function addNews() {
+  const data = JSON.parse(this.responseText);
+  /*const response = data.response;
+  console.log(data);*/
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+
+  let li = document.createElement('li');
+  li.className = 'articleClass';
+  li.innerText = snippet;
+
+  var newsIcon= document.createElement('i');
+  newsIcon.classList.add('far', 'fa-newspaper', 'newspaperIcon');
+  li.prepend(newsIcon);
+
+  responseContainer.appendChild(li);
+}
